4738f3ba42c858d8397b37e6f55eab97dfbf8ae6
ArtemDerenok <derenokartem95@gmail.com>
2023-03-05 19:28:39 +0300

    feat: add register validation

--- src/pages/registration/registration-page.tsx
+++ src/pages/registration/registration-page.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import Highlighter from 'react-highlight-words';
 import { useForm } from 'react-hook-form';
+import InputMask from 'react-input-mask';
 import { NavLink } from 'react-router-dom';
+import MaskedInput from 'react-text-mask';
+import { yupResolver } from '@hookform/resolvers/yup';
+import * as yup from 'yup';
 
 import arrowImg from '../../assets/arrow-right.png';
 import { Button } from '../../components/button/button';
@@ -10,10 +15,37 @@ import { changeStep } from '../../redux/slices/registration-slice';
 
 import styles from './registration-page.module.scss';
 
+const schema = yup
+  .object({
+    identifier: yup
+      .string()
+      .matches(/([A-Za-z])/g, 'латинский алфавит')
+      .matches(/([0-9])/g, 'цифры'),
+    password: yup
+      .string()
+      .min(8, 'не менее 8 символов')
+      .matches(/([A-ZА-Я]{1})/g, 'заглавной буквой')
+      .matches(/([0-9]{1})/g, 'цифрой')
+      .required('Поле не может быть пустым'),
+    firstName: yup.string().required('Поле не может быть пустым'),
+    lastName: yup.string().required('Поле не может быть пустым'),
+    phoneNumber: yup
+      .string()
+      .matches(
+        /^\+[1-9]{3}(\s+)?\(?([1-9][1-9])\)?(\s+)?[0-9]{3}-?[0-9]{2}-?[0-9]{2}$/,
+        'В формате +375 (xx) xxx-xx-xx'
+      )
+      .required('Поле не может быть пустым'),
+    email: yup.string().email('Введите корректный e-mail').required('Поле не может быть пустым'),
+  })
+  .required();
+
 export const RegistrationPage = () => {
   const {
     register,
     handleSubmit,
+    setError,
+    trigger,
     formState: { errors },
   } = useForm({
     defaultValues: {
@@ -24,6 +56,8 @@ export const RegistrationPage = () => {
       phoneNumber: '',
       email: '',
     },
+    resolver: yupResolver(schema),
+    mode: 'all',
   });
   const dispatch = useAppDispatch();
   const registration = useTypeSelector((state) => state.registrationReducer);
@@ -49,36 +83,136 @@ export const RegistrationPage = () => {
           {registration.step === 1 ? (
             <React.Fragment>
               <div className={styles.formContainer_form_firstInputContainer}>
-                <input type='text' placeholder='Придумайте логин для входа' {...register('identifier')} />
-                <p className={styles.formContainer_form_inputLabel}>Используйте для логина латинский алфавит и цифры</p>
+                <input
+                  type='text'
+                  placeholder='Придумайте логин для входа'
+                  {...register('identifier')}
+                  onBlur={async () => {
+                    const result = await trigger(['identifier']);
+
+                    if (!result) {
+                      setError('identifier', {
+                        type: 'custom',
+                        message: 'Используйте для логина латинский алфавит и цифры',
+                      });
+                    }
+                  }}
+                />
+                {errors.identifier?.message ? (
+                  <p className={`${styles.formContainer_form_inputLabel} ${styles.errorBorder}`}>
+                    <Highlighter
+                      highlightClassName={styles.errorColor}
+                      searchWords={[String(errors.identifier?.message)]}
+                      textToHighlight='Используйте для логина латинский алфавит и цифры'
+                    />
+                  </p>
+                ) : (
+                  <p className={styles.formContainer_form_inputLabel}>
+                    Используйте для логина латинский алфавит и цифры
+                  </p>
+                )}
               </div>
               <div className={styles.formContainer_form_secondInputContainer}>
-                <input type='password' placeholder='Пароль' {...register('password')} />
-                <p className={styles.formContainer_form_inputLabel}>
-                  Пароль не менее 8 символов, с заглавной буквой и цифрой
-                </p>
+                <input
+                  type='password'
+                  placeholder='Пароль'
+                  {...register('password')}
+                  onBlur={async () => {
+                    const result = await trigger(['password']);
+
+                    if (!result) {
+                      setError('password', {
+                        type: 'custom',
+                        message: 'Пароль не менее 8 символов, с заглавной буквой и цифрой',
+                      });
+                    }
+                  }}
+                />
+                {errors.password?.message ? (
+                  <p className={`${styles.formContainer_form_inputLabel} ${styles.errorBorder}`}>
+                    <Highlighter
+                      highlightClassName={styles.errorColor}
+                      searchWords={[String(errors.password?.message)]}
+                      textToHighlight='Пароль не менее 8 символов, с заглавной буквой и цифрой'
+                    />
+                  </p>
+                ) : (
+                  <p className={styles.formContainer_form_inputLabel}>
+                    Пароль не менее 8 символов, с заглавной буквой и цифрой
+                  </p>
+                )}
               </div>
-              <Button type='button' text='следующий шаг' handler={nextStep} />
+              <Button
+                type='button'
+                text='следующий шаг'
+                handler={async () => {
+                  const result = await trigger(['identifier', 'password']);
+
+                  if (result) {
+                    nextStep();
+                  }
+                }}
+              />
             </React.Fragment>
           ) : registration.step === 2 ? (
             <React.Fragment>
               <div className={styles.formContainer_form_firstInputContainer}>
                 <input type='text' placeholder='Имя' {...register('firstName')} />
+                {errors.firstName?.message ? (
+                  <p className={`${styles.formContainer_form_inputLabel} ${styles.error}`}>
+                    {errors.firstName?.message}
+                  </p>
+                ) : (
+                  <p className={styles.formContainer_form_inputLabel} />
+                )}
               </div>
               <div className={styles.formContainer_form_secondInputContainer}>
                 <input type='text' placeholder='Фамилия' {...register('lastName')} />
+                {errors.lastName?.message ? (
+                  <p className={`${styles.formContainer_form_inputLabel} ${styles.error}`}>
+                    {errors.firstName?.message}
+                  </p>
+                ) : (
+                  <p className={styles.formContainer_form_inputLabel} />
+                )}
               </div>
-              <Button type='button' text='последний шаг' handler={nextStep} />
+              <Button
+                type='button'
+                text='последний шаг'
+                handler={async () => {
+                  const result = await trigger(['firstName', 'lastName']);
+
+                  if (result) {
+                    nextStep();
+                  }
+                }}
+              />
             </React.Fragment>
           ) : registration.step === 3 ? (
             <React.Fragment>
               <div className={styles.formContainer_form_firstInputContainer}>
-                <input type='text' placeholder='Номер телефона' {...register('phoneNumber')} />
+                <InputMask
+                  mask='+999 (99) 999-99-99'
+                  type='tel'
+                  placeholder='Номер телефона'
+                  {...register('phoneNumber')}
+                />
+                {errors.phoneNumber?.message ? (
+                  <p className={`${styles.formContainer_form_inputLabel} ${styles.error}`}>
+                    {errors.phoneNumber?.message}
+                  </p>
+                ) : (
+                  <p className={styles.formContainer_form_inputLabel}>В формате +375 (xx) xxx-xx-xx</p>
+                )}
               </div>
               <div className={styles.formContainer_form_secondInputContainer}>
                 <input type='email' placeholder='E-mail' {...register('email')} />
+                {errors.email?.message ? (
+                  <p className={`${styles.formContainer_form_inputLabel} ${styles.error}`}>{errors.email?.message}</p>
+                ) : (
+                  <p className={styles.formContainer_form_inputLabel} />
+                )}
               </div>
-              {/* <Button type='submit' text='следующий шаг' handler={nextStep} /> */}
               <button type='submit' className={styles.button}>
                 Зерегистрироваться
               </button>
